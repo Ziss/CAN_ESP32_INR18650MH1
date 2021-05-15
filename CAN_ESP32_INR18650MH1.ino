fb002a543a406e4ab3f22ab345df9f6b1e377c9c
Ziss <zziiss@gmail.com>
2021-05-15 19:46:04 +0200

    Batterie structure and cleaner serial display

--- CAN_ESP32_INR18650MH1.ino
+++ CAN_ESP32_INR18650MH1.ino
@@ -7,6 +7,45 @@
 
 #include <CAN.h>
 
+char* can_func[]={\
+  "NMT  ",\
+  "SY/EY",\
+  "TIME ",\
+  "PDO1T",\
+  "PDO1R",\
+  "PDO2T",\
+  "PDO2R",\
+  "PDO3T",\
+  "PDO3R",\
+  "PDO4T",\
+  "PDO4R",\
+  "SDO-T",\
+  "SDO-R",\
+  "UKNWN",\
+  "HRTBT"\
+  };
+
+typedef struct
+{
+  uint32_t id;
+  uint8_t  data[8];
+  uint8_t  len;
+} CAN_msg_t;
+
+typedef struct
+{
+  uint16_t voltage=0; //in mV -- 0x2000 0x01
+  uint8_t  charge=0; //in % -- 0x2010 0x01
+  uint16_t maxcapacity; //in mAh -- 0x2010 0x02
+  int16_t  current=0; // in mA -- 0x2000 0x03 
+  bool updated=false;
+} Battery_val;
+
+Battery_val batt; //Battery values
+
+/******************************************************************************************/
+
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
@@ -30,25 +69,58 @@ void setup() {
 }
 
 void loop() {
-  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+
+  //testReadCANSerialRaw(); // Switch ON, OFF and ask battery charge. Dump raw data to Serial
+  //testReadCANSerialCANopen(); // Switch ON, OFF and ask battery charge. Parse CANopen to Serial
+  testReadCANSerialBatt(); // Switch ON and fetch battery info, print to Serial
+}
+
+/***************************************************************************************************************/
+
+void testReadCANSerialRaw(){ // Switch ON, OFF and ask battery charge. Dump raw data to Serial
   Serial.print("Sending packet ON ... ");
   switchON();
   Serial.println("done");
-  readCANSerial(3000);
+  readCANSerialBatt(3000);
 
   Serial.print("Sending packet BATT ... ");
   requestBattCharge();
   Serial.println("done");
-  readCANSerial(3000);
+  readCANSerialBatt(3000);
   
   Serial.print("Sending packet OFF ... ");
   switchOFF();
   Serial.println("done");
-  readCANSerial(3000);
-
+  readCANSerialBatt(3000);
 }
 
-/***************************************************************************************************************/
+void testReadCANSerialCANopen(){ // Switch ON, OFF and ask battery charge. Parse CANopen to Serial
+  Serial.print("Sending packet ON ... ");
+  switchON();
+  Serial.println("done");
+  readCANSerialCANopen(3000);
+
+  Serial.print("Sending packet BATT ... ");
+  requestBattCharge();
+  Serial.println("done");
+  readCANSerialCANopen(3000);
+  
+  Serial.print("Sending packet OFF ... ");
+  switchOFF();
+  Serial.println("done");
+  readCANSerialCANopen(3000);
+}
+
+void testReadCANSerialBatt() { // Switch ON and fetch battery info, print to Serial
+  switchON();
+  readCANSerialBatt(3000);
+  getIndex(0x200001); //voltage in mV
+  getIndex(0x201001); //charge in % 
+  getIndex(0x201002); // maxcapacity in mAh 
+  getIndex(0x200003); // current in mA
+  
+  readCANSerialBatt(3000);
+}
 
 void switchON() {
   CAN.beginPacket(0x630); //SDO-Receive to 0X30 (Battery ID)
@@ -76,8 +148,23 @@ void switchOFF() {
   CAN.endPacket();
 }
 
-void requestBattCharge() {
+void getIndex(uint32_t index) {
   CAN.beginPacket(0x630); //
+  CAN.write(0x40); //Upload
+  CAN.write((index & 0xFF00)>>8);
+  CAN.write((index & 0xFF0000)>>16);
+  CAN.write(index & 0xFF);
+  CAN.write(0x00);
+  CAN.write(0x00);
+  CAN.write(0x00);
+  CAN.write(0x00);
+  CAN.endPacket();
+}
+
+
+void requestBattCharge() {
+  getIndex(0x201001);
+/*  CAN.beginPacket(0x630); //
   CAN.write(0x40);
   CAN.write(0x10);
   CAN.write(0x20);
@@ -86,10 +173,10 @@ void requestBattCharge() {
   CAN.write(0x00);
   CAN.write(0x00);
   CAN.write(0x00);
-  CAN.endPacket();
+  CAN.endPacket();*/
 }
 
-void readCANSerial(unsigned int millisec) { //millisec: time to spend waiting for CAN messages
+void readCANSerialRaw(unsigned int millisec) { //millisec: time to spend waiting for CAN messages
 
     unsigned long previousMillis = millis();
     while(millis()- previousMillis < millisec) {
@@ -120,5 +207,126 @@ void readCANSerial(unsigned int millisec) { //millisec: time to spend waiting fo
         Serial.println();
         digitalWrite(LED_BUILTIN, LOW); 
       }
+  }
+}
+
+void readCANSerialCANopen(unsigned int millisec) { //millisec: time to spend waiting for CAN messages
+
+    unsigned long previousMillis = millis();
+    while(millis()- previousMillis < millisec) {
+      int packetSize = CAN.parsePacket();
+      if (packetSize) {
+        // received a packet
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
+        CAN_msg_t CAN_RX_msg;
+        CAN_RX_msg.id = CAN.packetId();
+        CAN_RX_msg.len = packetSize;
+        Serial.print("COB-ID: 0x");
+        if (CAN_RX_msg.id < 0x100) Serial.print("0");
+        if (CAN_RX_msg.id < 0x10) Serial.print("00");
+        Serial.print(CAN_RX_msg.id, HEX);
+        Serial.print(" ");
+      
+        int f = CAN_RX_msg.id & 0x780;
+        f = f >> 7;
+        Serial.print(can_func[f]);
+        Serial.print(" ");
+        int n = CAN_RX_msg.id & 0x7F;
+        Serial.print(n, HEX);
+        Serial.print(" ");
+      
+        Serial.print(" DLC: ");
+        Serial.print(CAN_RX_msg.len);
+        Serial.print(" Data: ");
+
+        int j = 0;
+        while (CAN.available()) {
+            CAN_RX_msg.data[j++] = CAN.read();
+          }
+        
+       for(int i=0; i<CAN_RX_msg.len; i++) {
+        Serial.print(CAN_RX_msg.data[i], HEX);
+        Serial.print(" ");
+       }
+      Serial.println();
+   }
+   digitalWrite(LED_BUILTIN, LOW); 
+   }
+}
+
+void readCANSerialBatt(unsigned int millisec) { //millisec: time to spend waiting for CAN messages
+  readCANBatt(millisec, &batt);
+  serial_Batt(&batt);
+}
+
+
+void serial_Batt(Battery_val* b)
+{
+    Serial.print("Charge: ");
+    Serial.print(b->charge);
+    Serial.println(" %");
+    Serial.print("Voltage: ");
+    Serial.print(b->voltage);
+    Serial.println(" mV");
+    Serial.print("Current: ");
+    Serial.print((int16_t)b->current);
+    Serial.println(" mA");
+    Serial.print("Max Capacity: ");
+    Serial.print((int16_t)b->maxcapacity);
+    Serial.println(" mAh");
+    Serial.println();
+}
+
+void readCANBatt(unsigned int millisec, Battery_val* b) { //millisec: time to spend waiting for CAN messages
+
+  unsigned long previousMillis = millis();
+  while(millis()- previousMillis < millisec) {
+    int packetSize = CAN.parsePacket();
+    if (packetSize) {
+      // received a packet
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
+      CAN_msg_t CAN_RX_msg;
+      CAN_RX_msg.id = CAN.packetId();
+      CAN_RX_msg.len = packetSize;
+      int j = 0;
+      while (CAN.available()) {
+        CAN_RX_msg.data[j++] = CAN.read();
+        }
+
+      if (CAN_RX_msg.id == 0x580 + 0x30){ //SDO-T from 0x30
+        int index = CAN_RX_msg.data[2]*0x10000 + CAN_RX_msg.data[1]*0x100 + CAN_RX_msg.data[3]; // Build index+subindex
+        switch (index) {
+          case 0x201001: //Percent level of charge  State of charge in percent. Calculated as 2000x04 / 2010x02 
+            b->charge = CAN_RX_msg.data[4];
+            break;
+  
+          case 0x200001: //Pack voltage in mV 
+            b->voltage = CAN_RX_msg.data[5]*0x100 + CAN_RX_msg.data[4];
+            break;
+  
+          case 0x200003: //Current +in -out 
+            b->current = CAN_RX_msg.data[5]*0x100 + CAN_RX_msg.data[4];
+            break;
+
+          case 0x201002: //Max Capacity in mAh 
+            b->maxcapacity = CAN_RX_msg.data[5]*0x100 + CAN_RX_msg.data[4];
+            break;
+  
+          case 0x220001: //Power output enabled. Cool!
+            break;
+          
+          default:
+            Serial.print("INDEX: ");
+            Serial.print(index, HEX);
+            Serial.print("  DATA: ");
+            for(int i=0; i<CAN_RX_msg.len; i++) {
+              Serial.print(CAN_RX_msg.data[i], HEX);
+              Serial.print(" ");
+            }
+            Serial.println();
+        }
+       }
+     digitalWrite(LED_BUILTIN, LOW); 
+     }
   }
 }
